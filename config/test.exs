27a915412118325510b1b7187e0e4a91b7b0b7e1
Adriano Lisboa <adrianolic@gmail.com>
2020-04-11 07:41:07 -0300

    Add postgres configuration for github actions

--- config/test.exs
+++ config/test.exs
@@ -1,12 +1,19 @@
 use Mix.Config
 
-# Configure your database
+# Configure your database for local testing
 config :didata, Didata.Repo,
   username: "postgres",
   password: "postgres",
   database: "didata_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+# Configure the database for GitHub Actions
+if System.get_env("GITHUB_ACTIONS") do
+  config :didata, Didata.Repo,
+    username: "postgres",
+    password: "postgres"
+end
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
